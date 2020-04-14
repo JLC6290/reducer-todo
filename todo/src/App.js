63feada47a57f11869b90daecda9e0f6b8edda66
James <jlc6290@gmail.com>
2020-04-14 11:41:01 -0500

    MVP completed

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,32 @@
+export const initialState = [
+    {
+        item: 'Task 0',
+        completed: false,
+        id: Date.now()
+    }
+]
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            const obj = {
+                item: action.payload,
+                completed: false,
+                id: Date.now()
+            };
+            return [...state, obj]
+
+
+        case 'TOGGLE':
+            return state.map(item => {
+                return item.id === action.payload ? { ...item, completed: !item.completed } : item
+            });
+
+            
+        case 'CLEAR':
+            return state.filter(item => !item.completed);
+        default:
+            return state
+
+
+    }
+}
